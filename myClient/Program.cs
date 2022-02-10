8c386eb26474934df76ab12441dd7bfba281d092
Szymon <39997113+szymon221@users.noreply.github.com>
2022-02-10 19:22:30 +0000

    Nicer Lab1

--- myClient/Program.cs
+++ myClient/Program.cs
@@ -1,80 +1,64 @@
 ﻿using System;
-using System.Net.Sockets;
 using System.IO;
+using System.Net.Sockets;
 public class Whois
 {
+    static TcpClient client = new TcpClient();
+    static StreamWriter sw;
+    static StreamReader sr;
+
     static void Main(string[] args)
     {
 
-        if (args.Length > 3) {
-
-            Console.WriteLine("Maximum of 2 arguments expected");
-            return;
-        }
-
-        if (args.Length < 1) {
-
-            Console.WriteLine("Minimum of 1 argument expected");
-            return;
-        }
-
-
-        if (args.Length == 1) {
-
-            query(args[0]);
-            //throw exception
-
-            return;
-        }
-
-        if (args.Length == 2)
+        if (args.Length > 3 || args.Length < 1)
         {
-            update(args[0], args[1]);
+            Console.WriteLine("Please supply 1 or 2 arguments");
+            return;
         }
-      
+
+        if (args.Length == 1)
+        {
+            query(args[0]);
+            return;
+        }
+
+        update(args[0], args[1]);
+
     }
 
+    static void update(string user, string data)
+    {
 
-    static void update(string user, string data) {
-
-        TcpClient client = new TcpClient();
         client.Connect("whois.net.dcs.hull.ac.uk", 43);
-        StreamWriter sw = new StreamWriter(client.GetStream());
-        StreamReader sr = new StreamReader(client.GetStream());
-
-
+        sw = new StreamWriter(client.GetStream());
+        sr = new StreamReader(client.GetStream());
         sw.WriteLine($"{user} {data}");
         sw.Flush();
-       
-        
 
         string reply = sr.ReadToEnd();
-
 
         if (reply == "OK\r\n")
         {
             Console.WriteLine($"{user} location changed to be {data}");
         }
-        else {
-
+        else
+        {
             Console.WriteLine(reply);
         }
-        
 
     }
 
 
-    static void query(string q) {
+    static void query(string q)
+    {
 
-        TcpClient client = new TcpClient();
         client.Connect("whois.net.dcs.hull.ac.uk", 43);
-        StreamWriter sw = new StreamWriter(client.GetStream());
-        StreamReader sr = new StreamReader(client.GetStream());
+        sw = new StreamWriter(client.GetStream());
+        sr = new StreamReader(client.GetStream());
         sw.WriteLine(q);
         sw.Flush();
         Console.WriteLine($"{q} is {sr.ReadToEnd()}");
 
     }
-
 
 }
