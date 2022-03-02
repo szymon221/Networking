31113a971b92d09a9ffa254da146a39fd438754d
Szymon <39997113+szymon221@users.noreply.github.com>
2022-03-02 19:58:45 +0000

    Created requests class

---
+++ locaitonserver/Request.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace locationserver
+{
+    public class Request
+    {
+        public readonly RequestType Type;
+        public readonly Ptcl Protocol;
+        public readonly string User;
+        public readonly string Location;
+
+        private readonly StreamReader sr;
+        private readonly StreamWriter sw;
+
+
+
+        public Request(TcpClient Client) {
+
+            sw = new StreamWriter(Client.GetStream());
+            sr = new StreamReader(Client.GetStream());
+
+            string Request = ReadRequest(sr);
+            Protocol = Ptcl.GetProtocol(Request.Split("\r\n")[0],sw);
+
+            Type = Protocol.GetRequestType(Request);
+
+            User = Protocol.SetUser(Request);
+            if (Type.GetType() == typeof(RequestUpdate))
+            {
+                Location = Protocol.SetLocation(Request);
+            }
+        }
+
+        private string ReadRequest(StreamReader sr)
+        {
+            string Response = "";
+            try
+            {
+                while (!sr.EndOfStream)
+                {
+                    Response += (char)sr.Read();
+                }
+            }
+            catch(IOException){}
+
+            return Response;
+        }
+    }
+
+    public  class RequestType 
+    {    
+    }
+    public class RequestUpdate : RequestType
+    { 
+    }
+
+    public class RequestLookup : RequestType
+    { 
+    }
+
+
+}
