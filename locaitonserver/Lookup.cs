dc4eec2c0151d9b222fc63cb2f4898dc3f5e79bc
Szymon <39997113+szymon221@users.noreply.github.com>
2022-03-02 19:57:39 +0000

    Created Lookup

---
+++ locaitonserver/Lookup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Concurrent;
+
+namespace locationserver
+{
+    class Lookup
+    {
+        private static Lookup _lookup;
+        public static Lookup GetInstance
+        {
+            get
+            {
+                if (_lookup == null)
+                {
+                    _lookup = new Lookup();
+                }
+                return _lookup;
+            }
+        }
+        public ConcurrentDictionary<string, string> _Location = new ConcurrentDictionary<string, string>();
+
+        public Lookup() {
+
+        }
+
+        public ConcurrentDictionary<string, string> Location { get { return _Location; } }
+
+        public void AddUser(string User) 
+        {
+
+        }
+
+
+    }
+}
