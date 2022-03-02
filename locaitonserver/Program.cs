c959c6f7a7d6d19fd81f620d0d113d5d44f2885f
Szymon <39997113+szymon221@users.noreply.github.com>
2022-03-02 19:58:18 +0000

    Redsigned from the ground up

--- locaitonserver/Program.cs
+++ locaitonserver/Program.cs
@@ -1,187 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.IO;
-using System.Net;
 using System.Net.Sockets;
-using System.Text;
+using locationserver;
+using System.Threading;
+using System.Net;
+
 namespace locaitonserver
 {
     class Program
     {
-
-
-        static readonly int PORT = 43;
-        static Socket listener;
-        static Dictionary<string, string> LocationLookup = new Dictionary<string, string>();
-
-#if DEBUG
-        static string path = @"..\Logs\DebugServer.txt";
-#else
-        static string path = @"..\Logs\ReleaseServer.txt";       
-#endif
-
-
-        //Create global task queue
-        //Create global return queue
-
-        //Create all socket objects
-
+        static Settings ServerSettings;
+        private static Thread[] Threads;
+        private static ConcurrentQueue<TcpClient> ClientQueue = new ConcurrentQueue<TcpClient>();
 
         static void Main(string[] args)
         {
+            ServerSettings = new Settings(args);
 
-
-            try
+            if (ServerSettings.Graphical)
             {
-                //Register signal handler for ctr+c
-
-
-                // This text is added only once to the file.
-                if (!File.Exists(path))
-                {
-                    // Create a file to write to.
-                    File.Create(path);
-                }
-                log("Server has started");
-
-                bool serverOn = true;
-
-                setUpSocket();
-
-                setUpThreads();
-
-                while (serverOn)
-                {
-
-                    processRequest();
-                }
-
-
+                Console.WriteLine("Starting graphical enviroment");
+                Environment.Exit(0);
             }
-            catch (Exception e)
-            {
 
-                log(e.Message);
+            Threads = new Thread[ServerSettings.Threads];
+            for (int i = 0; i < ServerSettings.Threads; i++)
+            {
+                Threads[i] = new Thread(ClientRequestProcessor);
+                Threads[i].Start();        
+            }
+
+            TcpListener listener = new TcpListener(IPAddress.Any, ServerSettings.Port);
+
+            listener.Start();
+            while (Settings.ServerOn)
+            {
+                TcpClient cli = listener.AcceptTcpClient();
+                ClientQueue.Enqueue(cli);
             }
         }
 
-
-
-        static void setUpSocket()
+        public static void ClientRequestProcessor()
         {
+            TcpClient Client;
+            Lookup DBLookup = Lookup.GetInstance;
 
-            log("Starting socket setup");
-
-            try
+            while (Settings.ServerOn)
             {
-                IPHostEntry HOST = Dns.GetHostEntry("127.0.0.1");
-                IPAddress ipAddress = HOST.AddressList[0];
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, PORT);
+                Request ClientRequest;
 
-                listener = new Socket(localEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                listener.Bind(new IPEndPoint(ipAddress, PORT));
-                listener.Listen(20);
-                log("Completed socket setup");
-
-            }
-            catch
-            {
-
-                log("Error has occured");
-
-                Environment.Exit(-1);
-            }
-
-        }
-
-        static void processRequest()
-        {
-
-            log("entered scoket request");
-            try
-            {
-                Socket handler = listener.Accept();
-
-                log("Socket has been conneted to");
-
-                string data = "";
-                byte[] readBuffer = null;
-
-
-                //1024 bytes is all anyone will ever need
-                readBuffer = new byte[1024];
-                int total = handler.Receive(readBuffer);
-                data += Encoding.ASCII.GetString(readBuffer, 0, total);
-                log($"{data}");
-
-
-                string response = parseRequest(data);
-
-                handler.Send(Encoding.ASCII.GetBytes(response));
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-            }
-            catch
-            {
-
-                log("fucked it");
-            }
-
-
-        }
-
-        static string parseRequest(string data)
-        {
-
-
-            if (data.Split(" ").Length > 1)
-            {
-
-                //update
-                string user = data.Split(" ")[0];
-                string location = string.Join(" ", data.Split(" ")[1..]).Split("\r\n")[0];
-
-                if (LocationLookup.TryGetValue(user, out _))
-                {
-                    LocationLookup[user] = location;
-                    return "OK\r\n";
+                if (ClientQueue.IsEmpty) {
+                    Thread.Sleep(1);
+                    continue;
                 }
 
-                LocationLookup.Add(user, location);
-                return "OK\r\n";
+                if(!ClientQueue.TryDequeue( out Client))
+                {
+                    Thread.Sleep(1);
+                    continue;
+                }
 
+                Client.ReceiveTimeout = 20;
 
+                ClientRequest = new Request(Client);
+                try
+                {
+                    if (ClientRequest.Type.GetType() == typeof(RequestLookup))
+                    {
+                        string Location;
 
+                        if (!DBLookup.Location.TryGetValue(ClientRequest.User, out Location))
+                        {
+                            ClientRequest.Protocol.ErrorResponse();
+                            Client.Close();
+                            continue;
+                        }
+                        ClientRequest.Protocol.QueryRequest(Location);
+                        Client.Close();
+                        continue;
+                    }
+                    ClientRequest.Protocol.UpdateRequest(ClientRequest.User, ClientRequest.Location);
+                    Client.Close();
+                }
+                catch (IOException) { 
+                
+                }
             }
-
-            string loc;
-
-            if (!LocationLookup.TryGetValue(data.Split("\r\n")[0], out loc))
-            {
-                loc = "ERROR: no entries found\r\n";
-            }
-
-            return loc;
-        }
-
-
-        static void setUpThreads()
-        {
-            //Create threadpool
-
-        }
-
-
-
-        static void log(string message)
-        {
-
-            using (StreamWriter sw = File.AppendText(path))
-            {
-                sw.WriteLine($"{DateTime.Now} {message}");
-            }
-
-
         }
     }
 }
