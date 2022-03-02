9ba5e049279355d8b5c7d7ceedc0cabf6a8c72f2
Szymon <39997113+szymon221@users.noreply.github.com>
2022-03-02 19:57:27 +0000

    Created Settings

---
+++ locaitonserver/Settings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace locationserver
+{
+    public class Settings
+    {
+        public readonly bool Graphical;
+        public readonly bool Logging;
+        public readonly bool Debug;
+        public readonly int Threads = 5;
+        public readonly int Port = 43;
+
+        private static bool _ServerOn = true;
+        public static bool ServerOn { get { return _ServerOn; } set { } }
+
+        public static void TurnServerOff() {
+            _ServerOn = false;
+        }
+
+
+        public Settings(string[] Args)
+        {
+            for (int ArgCounter = 0; ArgCounter < Args.Length; ArgCounter++)
+            {
+                switch (Args[ArgCounter].ToLower())
+                {
+                    case ("-w"):
+                        Graphical = true;
+                        break;
+
+                    case ("-l"):
+                        Logger.EnableLogger();
+                        break;
+
+                    case ("-d"):
+                        DebugWriter.EnableDebug();
+                        break;
+
+                    default:
+                        Console.WriteLine($"Warning!: Unrecognised argument {Args[ArgCounter]}");
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
