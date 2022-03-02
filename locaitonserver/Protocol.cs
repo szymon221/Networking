7bc1bacf224b710b368d78c5daa542eda1c6523d
Szymon <39997113+szymon221@users.noreply.github.com>
2022-03-02 19:16:16 +0000

    Created skeleton

---
+++ locaitonserver/Protocol.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+
+namespace locationserver
+{
+    public abstract class Ptcl
+    {
+        public static Ptcl GetProtocol(string FirstLine,StreamWriter sw)
+        {
+            return null;
+        }
+
+        public abstract string QueryRequest(string User);
+        public abstract string UpdateRequest(string User, string Location);
+        public abstract string ErrorResponse();
+        public abstract string SetUser(string Request);
+        public abstract string SetLocation(string Request);
+        public abstract RequestType GetRequestType(string Request);
+
+    }
+
+    public class WhoIsProtocol : Ptcl
+    {
+        public override string ErrorResponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RequestType GetRequestType(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string QueryRequest(string User)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SetLocation(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SetUser(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string UpdateRequest(string User, string Location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class H9 : Ptcl
+    {
+        public override string ErrorResponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RequestType GetRequestType(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string QueryRequest(string User)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SetLocation(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SetUser(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string UpdateRequest(string User, string Location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class H0 : Ptcl
+    {
+        public override string ErrorResponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RequestType GetRequestType(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string QueryRequest(string User)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SetLocation(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SetUser(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string UpdateRequest(string User, string Location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class H1 : Ptcl
+    {
+        public override string ErrorResponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RequestType GetRequestType(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string QueryRequest(string User)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SetLocation(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SetUser(string Request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string UpdateRequest(string User, string Location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
