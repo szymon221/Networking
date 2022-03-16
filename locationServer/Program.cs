a6dfccd57962106ec05f415530c8942ea118cb9b
Szymon <39997113+szymon221@users.noreply.github.com>
2022-03-16 13:01:19 +0000

    Moved all server logic to Request manager

--- locationServer/Program.cs
+++ locationServer/Program.cs
@@ -1,103 +1,30 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
 
 namespace locationserver
 {
     class Program
     {
         static Settings ServerSettings;
-        private static Thread[] Threads;
-        private static readonly ConcurrentQueue<TcpClient> ClientQueue = new ConcurrentQueue<TcpClient>();
-        private static readonly ConcurrentDictionary<string, string> Lookup = new ConcurrentDictionary<string, string>();
-        
+        static RequestManager Manager;
+           
         static void Main(string[] args)
         {
-            ServerSettings = new Settings(args);
 
+            Manager = new RequestManager(new Settings(args));
             if (ServerSettings.Graphical)
             {
                 Console.WriteLine("Starting graphical enviroment");
                 Environment.Exit(0);
             }
 
-            Threads = new Thread[ServerSettings.Threads];
-            for (int i = 0; i < ServerSettings.Threads; i++)
-            {
-                Threads[i] = new Thread(ClientRequestProcessor);
-                Threads[i].Start();
-            }
-
-            TcpListener listener = new TcpListener(IPAddress.Any, ServerSettings.Port);
-
-            listener.Start();
-            while (Settings.ServerOn)
-            {
-                TcpClient cli = listener.AcceptTcpClient();
-                ClientQueue.Enqueue(cli);
-            }
+            StartServer();
         }
 
-        public static void ClientRequestProcessor()
+        public static void StartServer()
         {
-            Request ClientRequest;
-
-            while (Settings.ServerOn)
-            {
-                if (ClientQueue.IsEmpty)
-                {
-                    Thread.Sleep(1);
-                    continue;
-                }
-
-                if (!ClientQueue.TryDequeue(out TcpClient Client))
-                {
-                    Thread.Sleep(1);
-                    continue;
-                }
-
-                try
-                {
-                    ClientRequest = new Request(Client);
-                }
-                catch (InvalidProtocolExcpetion) 
-                {
-                    Client.Close();
-                    continue;
-                }
-                DebugWriter.Write($"Client conntected from {ClientRequest.IPAdress}");
-                try
-                {
-                    if (RequestType.IsLookup(ClientRequest.Protocol.Type))
-                    {
-                        if (!Lookup.TryGetValue(ClientRequest.User, out string Location))
-                        {
-                            ClientRequest.Protocol.ErrorResponse();
-                            Logger.Log(ClientRequest.IPAdress, "GET", ClientRequest.User, ClientRequest.Location, "404");
-                            Client.Close();
-                            continue;
-                        }
-
-                        ClientRequest.Protocol.QueryResponse(Location);
-                        Client.Close();
-                        Logger.Log(ClientRequest.IPAdress, "GET", ClientRequest.User, ClientRequest.Location, "200");
-                        continue;
-                    }
-
-                    if (RequestType.IsUpdate(ClientRequest.Protocol.Type))
-                    {
-                        Lookup.AddOrUpdate(ClientRequest.User, ClientRequest.Location, (key, oldValue) => ClientRequest.Location);
-                        ClientRequest.Protocol.UpdateResponse();
-                        Client.Close();
-                        Logger.Log(ClientRequest.IPAdress, "POST", ClientRequest.User, ClientRequest.Location, "200");
-                        continue;
-                    }
-                }
-                catch (IOException) { }
-            }
+            Manager.CreateThreads();
+            Manager.Start();
         }
+
     }
 }
