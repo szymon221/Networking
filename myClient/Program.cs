b7e424adf0adbe45b5b408f56be599f1e492e928
Szymon <39997113+szymon221@users.noreply.github.com>
2022-03-16 20:43:36 +0000

    misc

--- myClient/Program.cs
+++ myClient/Program.cs
@@ -1,17 +1,37 @@
 ﻿using location;
 using System;
-
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 public class WhoIsClient
 {
+    //Stolen from https://limbioliong.wordpress.com/2011/10/14/minimizing-the-console-window-in-c/
+
+    const Int32 SW_MINIMIZE = 6;
+    [DllImport("Kernel32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+    private static extern IntPtr GetConsoleWindow();
+
+    [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool ShowWindow([In] IntPtr hWnd, [In] Int32 nCmdShow);
 
 
+    private static void MinimizeConsoleWindow()
+    {
+        IntPtr hWndConsole = GetConsoleWindow();
+        ShowWindow(hWndConsole, SW_MINIMIZE);
+    }
+
+    [STAThread]
     static void Main(string[] args)
     {
 
         if (args.Length == 0)
         {
-            Console.WriteLine("Staring GUI...");
+            MinimizeConsoleWindow();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GraphicalUI());
             Environment.Exit(0);
         }
 
@@ -24,7 +44,7 @@ public class WhoIsClient
     {
         Client.SendRequest();
         Console.WriteLine(Client.GetResponse());
-        
+
     }
 
 }
