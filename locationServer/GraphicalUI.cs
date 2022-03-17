f5d81a1d9f184247340156b786161317e022b73c
Szymon <39997113+szymon221@users.noreply.github.com>
2022-03-17 02:27:29 +0000

    added gui

---
+++ locationServer/GraphicalUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace locationserver
+{
+    public partial class GraphicalUI : Form
+    {
+        //Stolen from https://stackoverflow.com/questions/5282588/how-can-i-bring-my-application-window-to-the-front
+        [DllImport("User32.dll")]
+        public static extern Int32 SetForegroundWindow(int hWnd);
+        public GraphicalUI()
+        {
+            InitializeComponent();
+        }
+
+        private void GraphicalUI_Load(object sender, EventArgs e)
+        {
+            SetForegroundWindow(Handle.ToInt32());
+        }
+    }
+}
